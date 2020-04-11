7f5de83053d8d49c2921a20fa62db19ca9625391
chenxy <chenxy19960202@gmail.com>
2020-04-11 21:49:44 +0800

    20200411

---
+++ src/jianZhiOffer/_6_reversePrint/Solution.java
@@ -0,0 +1,30 @@
+package jianZhiOffer._6_reversePrint;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode (int x) { val = x; }
+}
+
+public class Solution {
+    public int[] reversePrint(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+
+        while (head != null) {
+            list.add(0, head.val);
+            head = head.next;
+        }
+
+        int[] ret = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            ret[i] = list.get(i);
+        }
+
+        return ret;
+    }
+}

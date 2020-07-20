4942fe545c2c07f8b6d13de710812869bca2dadd
chenxiangyu <chenxy19960202@gmail.com>
2020-07-20 19:48:46 +0800

    202007201948

--- src/jianZhiOffer/_57_2_和为s的连续正数序列/Solution.java
+++
@@ -1,7 +0,0 @@
-package jianZhiOffer._57_2_和为s的连续正数序列;
-
-public class Solution {
-    public int[][] findContinuousSequence(int target) {
-
-    }
-}
---
+++ src/jianZhiOffer/_57_2有一个非常好的方法_和为s的连续正数序列/OptimalSolution.java
@@ -0,0 +1,7 @@
+package jianZhiOffer._57_2有一个非常好的方法_和为s的连续正数序列;
+
+public class OptimalSolution {
+    public static int[][] findContinuousSequence(int target) {
+
+    }
+}

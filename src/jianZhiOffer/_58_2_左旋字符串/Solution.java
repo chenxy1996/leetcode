f9566a8ef4db5d2011569fd60624500c93acdfb8
chenxiangyu <chenxy19960202@gmail.com>
2020-07-20 23:23:30 +0800

    202007202323

---
+++ src/jianZhiOffer/_58_2_左旋字符串/Solution.java
@@ -0,0 +1,13 @@
+package jianZhiOffer._58_2_左旋字符串;
+
+public class Solution {
+    public static String reverseLeftWords(String s, int n) {
+        return s.substring(n) + s.substring(0, n + 1);
+    }
+
+    public static void main(String[] args) {
+        String s = "abcdef";
+        int offset = 2;
+        System.out.println(reverseLeftWords(s, offset));
+    }
+}

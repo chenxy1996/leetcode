af12301f6d1464073aba8f2d478cafa2709d25ba
chenxy <chenxy19960202@gmail.com>
2020-07-24 09:42:16 +0800

    202007240942

--- src/面试金典/_01_字符串问题/_06_字符串压缩/Solution.java
+++ src/面试金典/_01_字符串问题/_06_字符串压缩/Solution.java
@@ -1,8 +1,33 @@
 package 面试金典._01_字符串问题._06_字符串压缩;
 
+// 字符串压缩。利用字符重复出现的次数，编写一种方法，
+// 实现基本的字符串压缩功能。比如，字符串aabcccccaaa会变为a2b1c5a3。
+// 若“压缩”后的字符串没有变短，则返回原先的字符串。你可以假设字符串中
+// 只包含大小写英文字母（a至z）。
 public class Solution {
     public String compressString(String S) {
-        // TODO
-        return null;
+        if (S == null || S.length() == 0) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        char cr = S.charAt(0);
+        int count = 1;
+        for (int i = 1; i <= S.length(); i++) {
+            char c = 0;
+            if (i == S.length() || (c = S.charAt(i)) != cr) {
+                sb.append(cr);
+                sb.append(count);
+                cr = c;
+                count = 1;
+            } else {
+                count++;
+            }
+        }
+        return sb.length() < S.length() ? sb.toString() : S;
+    }
+
+    public static void main(String[] args) {
+        String s = "abbccd";
+        System.out.println(new Solution().compressString(s));
     }
 }
---
+++ src/面试金典/_01_字符串问题/_07_旋转矩阵/Solution.java
@@ -0,0 +1,7 @@
+package 面试金典._01_字符串问题._07_旋转矩阵;
+
+public class Solution {
+    public void rotate(int[][] matrix) {
+
+    }
+}
